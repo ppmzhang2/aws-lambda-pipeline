3d97acc03cec72bcb514722b76462b888a42f5ef
ZHANG, Meng <stall-breezes.0t@icloud.com>
2024-10-03 16:19:35 +1300

    Add stream to S3 I/O
    - process-earthquake
      - save to S3 parquet
      - stream S3 I/O
    - add conda environment configuration

--- read-earthquake/main.py
+++ read-earthquake/main.py
@@ -1,4 +1,4 @@
-"""Read all CSV files in a bucket."""
+"""Read a CSV file from S3, convert it to Parquet, and save it back to S3."""
 
 import json
 import logging
@@ -7,80 +7,97 @@ import os
 from aws_lambda_typing.context import Context
 import boto3
 import duckdb
-import pandas as pd
 
 logging.basicConfig(level=logging.INFO)
 
 s3 = boto3.client("s3")
-CHUNK_SIZE = 10000  # Maximum number of rows to read at once (chunk size)
+
+
+def receive_s3_event(event: dict) -> tuple[str, str]:
+    """Receive an S3 event and extract the bucket and key."""
+    record = event["Records"][0]
+    bucket = record["s3"]["bucket"]["name"]
+    key = record["s3"]["object"]["key"]
+    return bucket, key
 
 
 def handler(event: dict, context: Context) -> dict:  # noqa: ARG001
     """Lambda handler triggered by S3 events."""
-    # Extract the bucket name and key (file path) from the event
-    record = event["Records"][0]
-    input_bucket = record["s3"]["bucket"]["name"]
-    key = record["s3"]["object"]["key"]
-    output_bucket = os.environ.get("OUTPUT_BUCKET", input_bucket)
+    bucket_in, key_in = receive_s3_event(event)
+    bucket_out = os.environ.get("OUTPUT_BUCKET", bucket_in)
+    key_out = f"{os.path.basename(key_in).replace('.csv', '.parquet')}"
+    s3path_csv = f"s3://{bucket_in}/{key_in}"
+    s3path_pq = f"s3://{bucket_out}/{key_out}"
 
-    logging.info(f"Record: {record}; Bucket: {input_bucket}; Key: {key}")
+    logging.info(f"Bucket: {bucket_in}; Key: {key_in}")
 
-    # Process the CSV file
     try:
-        logging.info(f"Processing file: {key}")
-
-        # Download the CSV file from S3 to Lambda's temp storage
-        local_file_path = os.path.join(
-            "/tmp",  # noqa: S108
-            os.path.basename(key),
-        )
-        s3.download_file(input_bucket, key, local_file_path)
-
-        # Process the CSV file in chunks to optimize memory usage
-        row_count = process_csv_in_chunks(local_file_path)
-
-        logging.info(f"File: {key}, Row count: {row_count}")
-
-        # Remove the local file after processing
-        os.remove(local_file_path)
-
-        # Prepare result text and upload it back to S3
-        result_text = f"Processed file {key}. Total rows: {row_count}"
-        result_key = f"results/{os.path.basename(key)}_row_count.txt"
-        s3.put_object(Body=result_text, Bucket=output_bucket, Key=result_key)
-        logging.info(f"Result saved to {result_key}")
+        logging.info(f"Processing file: {key_in}")
+        csv2pq(s3path_csv, s3path_pq)
+        logging.info(f"Parquet file saved to {s3path_pq}")
 
     except Exception as e:
-        logging.exception(f"Error processing file {key}")
+        logging.exception(f"Error processing file {key_in}")
         return {
             "statusCode": 500,
-            "body": json.dumps(f"Error processing file {key}: {e!s}"),
+            "body": json.dumps(f"Error processing file {key_in}: {e!s}"),
         }
 
     return {
-        "statusCode":
-        200,
-        "body":
-        json.dumps(
-            f"Successfully processed file {key} with {row_count} rows."),
+        "statusCode": 200,
+        "body": json.dumps(f"Saved Parquet for {key_in}."),
     }
 
 
-def process_csv_in_chunks(file_path: str) -> int:
-    """Process a CSV file in chunks and count the total rows using DuckDB."""
-    # Initialize DuckDB connection and process file in chunks
-    total_rows = 0
+def csv2pq(path_in: str, path_out: str) -> None:
+    """Convert a S3 CSV file to S3 Parquet with DuckDB's S3 streaming."""
     try:
-        con = duckdb.connect()
+        # Connect to DuckDB and process the CSV directly from S3
+        con = duckdb.connect(database=":memory:")
 
-        # Use Pandas to read CSV in chunks and register each chunk with DuckDB
-        for chunk in pd.read_csv(file_path, chunksize=CHUNK_SIZE):
-            query = "SELECT COUNT(*) FROM chunk"
-            con.register("chunk", chunk)
-            result = con.execute(query).fetchone()
-            total_rows += result[0]
+        # Load the httpfs extension to allow S3 access
+        con.execute("LOAD httpfs;")
+
+        # Streaming: DuckDB reads the CSV directly from S3 and writes the
+        # Parquet directly back to S3
+        con.execute(f"""
+            COPY (
+                SELECT
+                    id,
+                    mag,
+                    place,
+                    TIMESTAMP 'epoch' +
+                        time / 1000 * INTERVAL '1 second' AS time,
+                    TIMESTAMP 'epoch' +
+                        updated / 1000 * INTERVAL '1 second' AS updated,
+                    url,
+                    detail,
+                    felt,
+                    cdi,
+                    mmi,
+                    alert,
+                    status,
+                    tsunami,
+                    sig,
+                    net,
+                    code,
+                    sources,
+                    nst,
+                    dmin,
+                    rms,
+                    gap,
+                    magType,
+                    type AS category,
+                    title,
+                    longitude,
+                    latitude,
+                    depth
+               FROM read_csv_auto("{path_in}", header=True)
+            ) TO "{path_out}" (FORMAT 'parquet');
+        """)
+
+        logging.info(f"Parquet written to {path_out}")
 
     except Exception:
-        logging.exception(f"Error processing CSV file {file_path}")
-
-    return total_rows
+        logging.exception("Error converting CSV to Parquet")
+        raise
